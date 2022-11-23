bceff51c61123d58b7c71d498d02d78209b11325
Facofra <facundo_olleros@hotmail.com>
2022-11-22 22:19:50 -0300

    filtros creados, pero falta validar isbn como int

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,11 +3,32 @@ const Ejemplar = require('../database/models/ejemplar.js')
 const Genero = require('../database/models/genero.js')
 const Libro = require('../database/models/libro.js')
 const Editorial = require('../database/models/editorial.js')
-const { Sequelize } = require("sequelize");
+const { Sequelize,Op } = require("sequelize");
 
 
 const indexController = {
     index: async function(req, res) {
+        let autor = req.query.autor
+        let genero = req.query.genero
+        let editorial = req.query.editorial
+        let titulo = req.query.titulo
+        let isbn = req.query.isbn
+
+        let filters =[]
+
+        // HAY QUE VALIDAR QUE ISBN SEA NUMÉRICO, PERO NO NOS SALE PORQUE UNDEFINED, NAN, STRING, VACIO, ES JODIDO 
+                        // if ( typeof(isbn) != 'undefined' && isNaN(isbn) ) {
+                        //     return res.status(400).json({error: "isbn debe ser numerico"})
+                        // } 
+
+                        // isbn = Number(isbn)
+        // --------------------------------------------------------------------------------------------------------------
+
+        isbn && filters.push({'$Libro.isbn$': isbn }) 
+        autor && filters.push({'$Libro.Autor.nombre$': {[Op.iLike] :`%${autor}%`} })
+        genero && filters.push({'$Libro.Genero.nombre$': {[Op.iLike] :`%${genero}%`} }) 
+        editorial && filters.push({'$Libro.Editorial.nombre$': {[Op.iLike] :`%${editorial}%`} })
+        titulo && filters.push({'$Libro.titulo$': {[Op.iLike] :`%${titulo}%`} })
 
         const books = await Ejemplar.findAll({
             attributes: ["id"],
@@ -31,6 +52,9 @@ const indexController = {
                 [Sequelize.col('Libro.Autor.nombre')],
                 [Sequelize.col('Libro.titulo')]
             ],
+            where: {
+                [Op.and]: filters
+            }
         });
         
         res.json(books)
