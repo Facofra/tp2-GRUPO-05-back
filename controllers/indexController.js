185d90441cf7e739171f2f5bf92e8441d5410dd2
Facofra <facundo_olleros@hotmail.com>
2022-11-23 21:25:11 -0300

    funcion createFilters creada en carpeta utils para usar en todos los controllers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,31 +4,20 @@ const Genero = require('../database/models/genero.js')
 const Libro = require('../database/models/libro.js')
 const Editorial = require('../database/models/editorial.js')
 const { Sequelize,Op } = require("sequelize");
+const createFilters = require('../utils/createFilters.js')
 
 
 const indexController = {
     index: async function(req, res) {
-        let autor = req.query.autor
-        let genero = req.query.genero
-        let editorial = req.query.editorial
-        let titulo = req.query.titulo
-        let isbn = req.query.isbn
 
-        let filters =[]
+        
+        let filters;
+        try {
+            filters = createFilters(req.query)
+        } catch (error) {
+            return res.status(400).json({error:error.message})
+        }
 
-        // HAY QUE VALIDAR QUE ISBN SEA NUMÉRICO, PERO NO NOS SALE PORQUE UNDEFINED, NAN, STRING, VACIO, ES JODIDO 
-                        // if ( typeof(isbn) != 'undefined' && isNaN(isbn) ) {
-                        //     return res.status(400).json({error: "isbn debe ser numerico"})
-                        // } 
-
-                        // isbn = Number(isbn)
-        // --------------------------------------------------------------------------------------------------------------
-
-        isbn && filters.push({'$Libro.isbn$': isbn }) 
-        autor && filters.push({'$Libro.Autor.nombre$': {[Op.iLike] :`%${autor}%`} })
-        genero && filters.push({'$Libro.Genero.nombre$': {[Op.iLike] :`%${genero}%`} }) 
-        editorial && filters.push({'$Libro.Editorial.nombre$': {[Op.iLike] :`%${editorial}%`} })
-        titulo && filters.push({'$Libro.titulo$': {[Op.iLike] :`%${titulo}%`} })
 
         const books = await Ejemplar.findAll({
             attributes: ["id"],
---
+++ utils/createFilters.js
@@ -0,0 +1,32 @@
+const { Op } = require("sequelize");
+
+
+function createFilters(query){
+    let autor = query.autor
+    let genero = query.genero
+    let editorial = query.editorial
+    let titulo = query.titulo
+    let isbn = query.isbn
+
+    let filters =[]
+
+    // Checkear que isbn sea numero , aunque venga en string
+    const regIsNumber = /^\d+$/
+    if ( !regIsNumber.test(isbn) && typeof(isbn) != 'undefined' && isbn!="") {
+        throw new TypeError('isbn debe ser numérico')
+    } 
+
+    console.log(regIsNumber.test(isbn));
+    isbn = Number(isbn)
+
+    isbn && filters.push({'$Libro.isbn$': isbn }) 
+    autor && filters.push({'$Libro.Autor.nombre$': {[Op.iLike] :`%${autor}%`} })
+    genero && filters.push({'$Libro.Genero.nombre$': {[Op.iLike] :`%${genero}%`} }) 
+    editorial && filters.push({'$Libro.Editorial.nombre$': {[Op.iLike] :`%${editorial}%`} })
+    titulo && filters.push({'$Libro.titulo$': {[Op.iLike] :`%${titulo}%`} })
+
+    return filters
+
+}
+
+module.exports = createFilters;
